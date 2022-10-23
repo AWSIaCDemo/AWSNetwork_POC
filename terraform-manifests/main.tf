8d1cab10ccaf575b08817c13d43141db51b3293b
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-10-23 01:26:19 -0400

    Update main.tf

--- terraform-manifests/main.tf
+++ terraform-manifests/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   version = "3.0.0"
 
   # VPC Basic Details
-  #name = "${var.vpc_name}"
+  name = "Network-Prod-E1-VPC" 
   cidr = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
   #name = "Network-Prod-E1-Public-SNET"
@@ -17,13 +17,6 @@ module "vpc" {
   #name = "Network-Prod-E1-Private-SNET"
   private_subnets = var.vpc_private_subnets  
 
-  # Database Subnets
-  #database_subnets = var.vpc_database_subnets
-  #create_database_subnet_group = var.vpc_create_database_subnet_group
-  #create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  # create_database_internet_gateway_route = true
-  # create_database_nat_gateway_route = true
-  
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = var.vpc_enable_nat_gateway 
   single_nat_gateway = var.vpc_single_nat_gateway
